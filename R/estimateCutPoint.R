a240e5c25d133eaf5b3b718fe04bd1867d463c63
genomaths <18340747+genomaths@users.noreply.github.com>
2020-03-23 18:17:56 -0400

    Update vignette. Update function estimateCutPoint. The initial code for function 'estimateCutPoint' is now integrating two new functions: 'simpleCutPoint' and 'mlCutpoint'. Now, function 'estimateCutPoint' call these two functions

--- R/estimateCutPoint.R
+++ R/estimateCutPoint.R
@@ -150,9 +150,9 @@
 #' @seealso \code{\link[MethylIT]{evaluateDIMPclass}}
 #' @export
 #' @examples
-#' ## Get a dataset of potential signals and the estimated cutpoint from the
-#' ## package and performs cutpoint estimation
-#' data(PS, package = 'MethylIT')
+#' ## Get a dataset of potential signals and the estimates cutpoint
+#' ## from the package and performs cutpoint estimation
+#' data(PS)
 #' cutpoint = estimateCutPoint(LR = PS, simple = FALSE,
 #'                             column = c(hdiv = TRUE, TV = TRUE,
 #'                                         wprob = TRUE, pos = TRUE),
@@ -198,19 +198,15 @@ estimateCutPoint <- function(LR, control.names, treatment.names,
         }
     }
 
-    # --------------------------- valid 'pDMP'
-    # object-------------------------- #
+    # ----------------------- valid 'pDMP' bject-------------------------- #
     validateClass(LR)
     # -------------------------------------------------------------------------
-    # # -----------------------Divergences are
-    # positives ------------------------# In case that
-    # TV column would be used as source to get TVD
     vn <- c("hdiv", "TV")
     vn <- vn[match(TRUE, column[vn])]
     if (is.null(div.col))
         div.col <- vn
 
-    sn <- names(LR)
+    # sn <- names(LR)
     if (any(unlist(lapply(LR, function(GR) min(mcols(GR[,
         div.col])[, 1], na.rm = TRUE))) < 0)) {
         LR <- lapply(LR, function(GR) {
@@ -220,83 +216,28 @@ estimateCutPoint <- function(LR, control.names, treatment.names,
         }, keep.attr = TRUE)
     }
 
-    # -------------------------- Auxiliary functions
-    # -------------------------- #
-    infDiv <- function(LR, div.col = NULL) {
-        ## This builds data frames from the list or ranges
-        ## LR to be used for ROC analysis LR: list of sample
-        ## GRanges
-        if (is.null(div.col))
-            stop(paste("* Provide a divergence column"))
-
-        dt <- list(ctrl = data.frame(), treat = data.frame())
-        dt$ctrl <- data.frame(idiv = abs(mcols(LR$ctrl[,
-            div.col])[, 1]), treat = "ctrl")
-        dt$treat <- data.frame(idiv = abs(mcols(LR$treat[,
-            div.col])[, 1]), treat = "treat")
-        return(dt)
-    }
-    roc <- function(dt) {
-        ## This function build the ROC and estimate the
-        ## Hellinger divergence cutoff point starting from
-        ## which TRUE DMPs are found.  dt0 & dt1: data
-        ## frames built with HD function dt0: control dt1:
-        ## treatment folder: place where the ROC will be
-        ## printed
-        dt <- do.call(rbind, dt)
-        l <- levels(dt$treat)
-        dt$status <- as.character(dt$treat)
-        dt$status[dt$status == l[1]] <- 0
-        dt$status[dt$status == l[2]] <- 1
-        dt$status <- as.numeric(dt$status)
-        rownames(dt) <- NULL
-
-        m <- as.matrix(base::table(dt$idiv, dt$status))
-        m <- addmargins(rbind(0, m), 2)
-        nr <- nrow(m)
-        m <- apply(m, 2, cumsum)
-        sens <- (m[nr, 2] - m[, 2])/m[nr, 2]
-        spec <- m[, 1]/m[nr, 1]
-        auc <- sum((sens[-1] + sens[-nr])/2 * abs(diff(1 -
-            spec)))
-        # Youden Index
-        idx <- which.max(sens[-1] + spec[-nr])
-        return(list(cutp = as.numeric(names(idx)),
-            auc = auc, sens = sens[idx], spec = spec[idx]))
-    }
-
-    res <- list(cutpoint = NA, testSetPerformance = NA,
-        testSetModel.FDR = NA, model = NA, modelConfMatrix = NA,
-        initModel = NA, postProbCut = NA, postCut = NA,
-        classifier = NA, statistic = NA, optStatVal = NA,
-        cutpData = NA)
-    res <- structure(res, class = c("CutPoint", "list"))
-
-    # -------------------------------------------------------------------------
-    # #
+    ## --------------------------------------------------------------------##
     if (!is.null(control.names) && !is.null(treatment.names))
-        LR <- try(LR[c(control.names, treatment.names)],
-            silent = TRUE)
+        LR <- try(LR[c(control.names, treatment.names)], silent = TRUE)
     if (inherits(LR, "try-error"))
         stop("List's names does not match control & treatment names")
 
-    lcc <- unlist(lapply(control.names, function(k) length(LR[[k]]) >
-        0))
-    ltt <- unlist(lapply(treatment.names, function(k) length(LR[[k]]) >
-        0))
+    lcc <- unlist(lapply(control.names, function(k) length(LR[[k]]) > 0))
+    ltt <- unlist(lapply(treatment.names, function(k) length(LR[[k]]) > 0))
 
     if (sum(ltt) < length(LR[treatment.names])) {
         if (sum(ltt) == 0) {
-            text <- paste0("All the GRanges objects from treatment group are ",
-                "empty, please check your data")
+            text <- paste0("All the GRanges objects from treatment group are "
+                        , "empty, please check your data")
             stop(text)
         } else treatment.names <- treatment.names[ltt]
     }
 
     if (sum(lcc) == 0) {
         LR <- LR[treatment.names]
-        min.div <- min(unlist(lapply(LR, function(l) min(l@elementMetadata[,
-            div.col], na.rm = TRUE))), na.rm = TRUE)
+        min.div <- min(unlist(lapply(LR, function(l)
+                           min(l@elementMetadata[, div.col], na.rm = TRUE))),
+                        na.rm = TRUE)
         min.div <- min.div[min.div > 0]
         text <- c("All the GRanges objects from your control are empty ",
             "\n", "So, the cutpoint is the min(div) > 0 value found",
@@ -307,241 +248,43 @@ estimateCutPoint <- function(LR, control.names, treatment.names,
         control.names <- control.names[lcc]
         LR <- LR[c(control.names, treatment.names)]
 
-        # ========================== Grouping
-        # ==================================
-        LR = list(unlist(LR[control.names]), unlist(LR[treatment.names]))
-        names(LR) <- c("ctrl", "treat")
-        LR <- structure(LR, class = "pDMP")
-        if (is.null(tv.col))
-            tv.col = match("TV", colnames(mcols(LR[[1]])))
-        classes <- c(rep("CT", length(LR$ctrl)), rep("TT",
-            length(LR$treat)))
-        classes <- factor(classes, levels = c("CT",
-            "TT"))
-
+        # ======================== Grouping ========================== #
         if (simple) {
-            dt = infDiv(LR = LR, div.col = div.col)
-            cutp <- roc(dt)
-            cutpoint <- cutp$cutp
-            predClasses <- mcols(unlist(LR)[, div.col])[,
-                1] > cutpoint
-            predClasses[predClasses == TRUE] <- "TT"
-            predClasses[predClasses == FALSE] <- "CT"
-            predClasses <- factor(predClasses, levels = c("CT",
-                "TT"))
-            cf.mat <- try(confusionMatrix(data = predClasses,
-                reference = classes, positive = "TT"),
-                silent = TRUE)
+            res <- simpleCutPoint(LR = LR,
+                                column = column,
+                                classifier = classifier1, n.pc = n.pc,
+                                control.names = control.names,
+                                treatment.names = treatment.names,
+                                tv.col = tv.col, tv.cut = tv.cut,
+                                clas.perf = clas.perf, prop = prop,
+                                div.col = div.col, cutp_data = cutp_data,
+                                num.cores = num.cores, tasks = tasks, ...)
 
-            if (inherits(cf.mat, "try-error"))
-                stop("*** Cutpoint estimation based on Youden index failed. \n",
-                    "Only ", 100 * sum(dt$treat$idiv >
-                    cutpoint)/length(dt$treat$idiv),
-                    " percent of the cytosine positions from treatment are \n",
-                    "greater than the cutpoint value: ",
-                    round(cutpoint, 2), "\n\n  Other indicators are: \n",
-                    "sensitivity = ", round(cutp$sens,
-                    3), "\n", "specificity = ", round(cutp$spec,
-                    3), "\n", "auc = ", round(cutp$auc,
-                    4), "\n\n", "   Please try the optimal cutpoint estimation",
-                    " with machine-learning \n classifiers (simple = FALSE, ",
-                    "see examples given in \n the help for function:",
-                    " ?estimateCutPoint)")
-            res$cutpoint <- cutpoint
-            if (cutp_data)
-                res$cutpData <- dt
-
-            if (clas.perf) {
-                dmps <- selectDIMP(LR, div.col = div.col,
-                                    cutpoint = cutpoint, tv.col = tv.col,
-                                    tv.cut = tv.cut)
-
-                conf.mat <- evaluateDIMPclass(LR = dmps,
-                                    column = column, control.names = "ctrl",
-                                    treatment.names = "treat",
-                                    classifier = classifier1[1],
-                                    prop = prop, n.pc = n.pc,
-                                    output = "conf.mat",
-                                    num.cores = num.cores, tasks = tasks, ...)
-
-                predClasses <- predict(object = conf.mat$model,
-                                        newdata = dmps, type = "class")
-                predClasses <- factor(predClasses, levels = c("CT", "TT"))
-                classes <- c(rep("CT", length(dmps$ctrl)),
-                                rep("TT", length(dmps$treat)))
-                classes <- factor(classes, levels = c("CT", "TT"))
-                conf.matrix <- confusionMatrix(data = predClasses,
-                                        reference = classes, positive = "TT")
-
-                res$testSetPerformance <- conf.mat$Performance
-                res$testSetModel.FDR <- conf.mat$FDR
-                res$model <- conf.mat$model
-                res$modelConfMatrix <- conf.matrix
-                res$initModel <- "Youden Index"
-                res$initModelConfMatrix <- cf.mat
-                res$classifier <- classifier1[1]
-            } else {
-                res$modelConfMatrix <- cf.mat
-                res$initModel <- "Youden Index"
-            }
         } else {
-            # -------------------- To search for a cutpoint
-            # --------------------- #
-
-            if (is.null(classifier2[1]))
-                classifier2 <- classifier1[1]
-            conf.mat <- evaluateDIMPclass(LR, column = column,
-                control.names = "ctrl", treatment.names = "treat",
-                classifier = classifier1[1], prop = prop,
-                output = "conf.mat", n.pc = n.pc, interaction = interaction,
-                num.cores = num.cores, tasks = tasks,
-                ...)
-
-            post <- predict(object = conf.mat$model,
-                newdata = LR, type = "posterior")
-
-            if (classifier1[1] == "logistic" ||
-                classifier1[1] == "pca.logistic")
-                idx <- which(post > post.cut)
-            else idx <- which(post[, 2] > post.cut)
-            cutpoint <- min(mcols(unlist(LR)[idx, div.col])[, 1])
-            res$postCut <- cutpoint
-            cuts <- cutpoint
-            if (stat %in% seq_len(11))
-                st <- conf.mat$Performance$byClass[stat]
-            if (stat == 0)
-                st <- conf.mat$Performance$overall[1]
-            if (stat == 12)
-                st <- conf.mat$FDR
-
-            if (!is.null(cut.values)) {
-                if (is.numeric(cut.values)) {
-                    cuts <- sort(c(cut.values, cutpoint))
-                    if (max(cuts) > max(mcols(unlist(LR)[, div.col])[, 1])) {
-                        warning("Cut values supplied are",
-                                " > max Inf Div. Ignored")
-                        cuts <- cutpoint
-                    }
-                } else warning("Parameter cut.values must be numeric. Ignored")
-            }
-            if (length(cuts) > 1) {
-                k = 1
-                opt <- FALSE
-                overcut <- FALSE
-
-                while (k < length(cuts) && !opt && !overcut) {
-                    dmps <- selectDIMP(LR, div.col = div.col,
-                                    cutpoint = cuts[k], tv.col = tv.col,
-                                    tv.cut = tv.cut)
-                    min.div <- min(dmps$treat$hdiv, na.rm = TRUE)
-                    if (length(dmps$ctrl) > 0) {
-                        conf.mat <- evaluateDIMPclass(LR = dmps,
-                                                column = column,
-                                                control.names = "ctrl",
-                                                treatment.names = "treat",
-                                                classifier = classifier2[1],
-                                                prop = prop,
-                                                output = "conf.mat",
-                                                n.pc = n.pc,
-                                                num.cores = num.cores,
-                                                tasks = tasks, ...)
-                    if (stat == 0) {
-                        st0 <- conf.mat$Performance$overall[1]
-                        if (st < st0) {
-                            st <- st0
-                            cutpoint <- max(cuts[k], min.div,
-                                            na.rm = TRUE)
-                        }
-                        if (st == 1) opt <- TRUE
-                        k <- k + 1
-                    }
-                    if (stat %in% seq_len(11)) {
-                        st0 <- conf.mat$Performance$byClass[stat]
-                        if (st < st0) {
-                            st <- st0
-                            cutpoint <- max(cuts[k], min.div,
-                                            na.rm = TRUE)
-                        }
-                        if (st == 1) opt <- TRUE
-                        k <- k + 1
-                    }
-                    if (stat == 12) {
-                        st0 <- conf.mat$FDR
-                        if (st > st0) {
-                            st <- st0
-                            cutpoint <- max(cuts[k], min.div, na.rm = TRUE)
-                        }
-                        if (st == 0) opt <- TRUE
-                        k <- k + 1
-                    }
-                    } else {
-                        overcut <- TRUE
-                        if (k == 1) {
-                            st <- conf.mat$Performance$byClass[stat]
-                            warning("Model classifier ",
-                                    classifier1[1], " is enough")
-                        }
-                    }
-                }
-            } else {
-                dmps <- selectDIMP(LR, div.col = div.col,
-                                    cutpoint = cutpoint, tv.col = tv.col,
-                                    tv.cut = tv.cut)
-                conf.mat <- evaluateDIMPclass(LR = dmps,
-                                    column = column, control.names = "ctrl",
-                                    treatment.names = "treat",
-                                    classifier = classifier2[1],
-                                    prop = prop, output = "conf.mat",
-                                    n.pc = n.pc, interaction = interaction,
-                                    num.cores = num.cores, tasks = tasks, ...)
-                if (stat == 0)  st <- conf.mat$Performance$overall[1]
-                if (stat %in% seq_len(11))
-                    st <- conf.mat$Performance$byClass[stat]
-            }
-            predClasses <- predict(object = conf.mat$model,
-                newdata = dmps, type = "class")
-            predClasses <- factor(predClasses, levels = c("CT", "TT"))
-            classes <- c(rep("CT", length(dmps$ctrl)),
-                rep("TT", length(dmps$treat)))
-            classes <- factor(classes, levels = c("CT", "TT"))
-            conf.matrix <- confusionMatrix(data = predClasses,
-                reference = classes, positive = "TT")
-
-            STAT <- c("Accuracy", "Sensitivity", "Specificity",
-                "Pos Pred Value", "Neg Pred Value",
-                "Precision", "Recall", "F1", "Prevalence",
-                "Detection Rate", "Detection Prevalence",
-                "Balanced Accuracy", "FDR")
-
-            res$cutpoint <- cutpoint
-            res$testSetPerformance <- conf.mat$Performance
-            res$testSetModel.FDR <- conf.mat$FDR
-            res$model <- conf.mat$model
-            res$modelConfMatrix <- conf.matrix
-            res$initModel <- classifier1[1]
-            res$classifier <- classifier2[1]
-            res$statistic <- STAT[stat + 1]
-            res$postProbCut <- post.cut
-            res$optStatVal <- st
+            res <- mlCutpoint(LR = LR,
+                            control.names = control.names,
+                            treatment.names = treatment.names,
+                            column = column, div.col = div.col,
+                            tv.col = tv.col, tv.cut = tv.cut,
+                            post.cut = post.cut,
+                            classifier1 = classifier1,
+                            classifier2 = classifier2,
+                            n.pc = n.pc, prop = prop,
+                            stat = stat, cut.values = cut.values,
+                            num.cores = num.cores, tasks = tasks, ...)
         }
     }
     return(res)
 }
 
-
-### ========================== Auxiliary function
-### ============================ #
-
 #' @aliases print.CutPoint
 #' @keywords internal
 #' @export
-print.CutPoint <- function(x, digits = getOption("digits"),
-    ...) {
+print.CutPoint <- function(x, digits = getOption("digits"), ...) {
 
     postProbCut <- format(signif(x$postProbCut, max(1L,
         digits - 2L)))
-    cutpoint <- format(signif(x$cutpoint, max(1L, digits -
-        2L)))
+    cutpoint <- format(signif(x$cutpoint, max(1L, digits - 2L)))
 
     if (x$initModel != "Youden Index") {
         cat("Cutpoint estimation with '", x$initModel,
@@ -552,8 +295,7 @@ print.CutPoint <- function(x, digits = getOption("digits"),
     }
     if (!is.na(x$statistic)) {
 
-        sta.val <- format(signif(x$optStatVal, max(1L,
-            digits - 2L)))
+        sta.val <- format(signif(x$optStatVal, max(1L, digits - 2L)))
 
         cat("Cutpoint search performed using model posterior probabilities \n")
         cat("\n")
