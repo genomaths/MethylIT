ce79c943b43e79a94b11b340a95dd354986eff69
genomaths <18340747+genomaths@users.noreply.github.com>
2020-04-02 21:39:48 -0400

    Update testthat

--- tests/testthat/test_countTest2.R
+++ tests/testthat/test_countTest2.R
@@ -5,13 +5,8 @@ library(MethylIT)
 context("countTest2 tests")
 
 test_that("countTest2 test", {
-  countData <- matrix(1:40,ncol = 4)
-  colnames(countData) <- c("A1","A2","B1","B2")
-  colData <- data.frame(condition = factor(c("A","A","B","B")),
-                        c("A1","A2","B1","B2"),
-                        row.names = 2)
-  ds <- glmDataSet(counts = countData, colData = colData)
-  y <- countTest2(DS=ds, num.cores = 1L, maxGrpCV = c(0.6, 0.5),
-                   verbose = FALSE)
-  expect_true(all(y$adj.pval < 0.1))
+    data(ds, package = "MethylIT")
+    y <- countTest2(DS = ds, num.cores = 1L, maxGrpCV = c(0.6, 0.5),
+                    verbose = FALSE)
+    expect_true(all(y$adj.pval < 0.1))
 })
